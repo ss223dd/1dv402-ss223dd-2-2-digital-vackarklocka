adc27f3a5cf892c072a27958db1738fff80e1232
Sebastian Ström <ss223dd@student.lnu.se>
2014-10-21 19:54:41 +0200

    Removed the over-extensive comments and made them into a short, overall summary. Ready for presentation.

--- 1DV402.S2.L02A/1DV402.S2.L02A/AlarmClock.cs
+++ 1DV402.S2.L02A/1DV402.S2.L02A/AlarmClock.cs
@@ -8,14 +8,15 @@ namespace _1DV402.S2.L02A
 {
     class AlarmClock
     {
-        private int _alarmHour; // Private field that contains the value of the alarm hour. Encapsulated by the property "AlarmHour".
-        private int _alarmMinute; // Contains the value of the alarm minute. Encapsulated by the property "AlarmMinute".
-        private int _hour; // Contains the hour value of the current time. Encapsulated by "Hour".
-        private int _minute; // Contains the minute value of the current time. Encapsulated by "Minute".
+        /// <summary>
+        /// Class verifying that an "AlarmClock-object" is correctly initialized with valid values, making the clock turn one minute (whilst checking if the new time
+        /// complies to the time of the alarm) and returns a string-representation of an AlarmClock instance.
+        /// </summary>
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
 
-        // Property which encapsulates the private field "_alarmHour". The set method needs to verify that the value to be assigned to _alarmHour 
-        // validates within the interval of 0-23. If out of the required interval - an ArgumentException should be thrown.
-        // Same with props: AlarmMinute (0-59), Hour (0-23) and Minute (0-59).
         public int AlarmHour
         {
             get { return _alarmHour; }
@@ -68,26 +69,18 @@ namespace _1DV402.S2.L02A
             }
         }
 
-        // 3 constructors which task is to verify that an "AlarmClock-object" is correctly initialized with valid values/field data.
-
-        // The default constructor should initialize the fields with their default values.
-        // However no assignment is allowed within its body, which also should be empty. Need to call the constructor with two parameters.
         public AlarmClock()
             : this(0, 0)
         {
             
         }
 
-        // This constructor should enable an AlarmClock-object to be initialized with its hour and minute.
-        // However no assignment is allowed within its body, which also should be empty. Need to call the constructor with four parameters.
         public AlarmClock(int hour, int minute)
             : this(hour, minute, 0, 0)
         {
             
         }
 
-        // Finally, the constructor which enables an AlarmClock-object to be initialized with its time and time of the alarm, given within the parameters.
-        // The only constructor where assigning values to the fields is allowed within its body.
         public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
         {
             Hour = hour;
@@ -96,10 +89,6 @@ namespace _1DV402.S2.L02A
             AlarmMinute = alarmMinute;
         }
         
-        // Method being called to make the clock turn one minute. If the new time complies to the time of the alarm, the method should return "true", otherwise "false".
-        // Method is responsible for increasing the minute value by 1 and the value has to be within 0-59.
-        // When the value reaches 0, the value of hour should increase by 1. Value of hours has to be within 0-23.
-        // No "Console.Write(Line)'s" allowed in this method.
         public bool TickTock()
         {
             _minute++;
@@ -124,10 +113,6 @@ namespace _1DV402.S2.L02A
             }
         }
         
-        // Method which task is to return a string representing the value of an instance of the AlarmClock class (an object).
-        // In case the value of the hour is only ONE(1) integer/digit, it should be presented WITHOUT THE COMMENCING ZERO.
-        // However if the minute value is just one digit - the commencing zero should be presented.
-        // No "Console.Write(Line)'s" allowed in this method.
         public override string ToString()
         {
             return (string.Format("{0,3}:{1:d2} ({2}:{3:d2})", _hour, _minute, _alarmHour, _alarmMinute));
--- 1DV402.S2.L02A/1DV402.S2.L02A/Program.cs
+++ 1DV402.S2.L02A/1DV402.S2.L02A/Program.cs
@@ -8,54 +8,46 @@ namespace _1DV402.S2.L02A
 {
     class Program
     {
-        // Simply a line (across the whole console window) seperating the different tests.
-        private static string HorizontalLine = "═══════════════════════════════════════════════════════════════════════════════";
+        /// <summary>
+        /// This class is responsible for testing the AlarmClock class, verifying that its members have been correctly implemented 
+        /// by running a total of 7 tests, in which different AlarmClock-objects are being instantiated and used to test the constructors, 
+        /// properties and methods respectively. Furthermore, the class will also present the results of the tests, including any potential 
+        /// error messages being thrown.
+        /// </summary>
 
-        // Method which instantiates "AlarmClock-objects" and tests the constructors, the properties and the methods. 7 tests in total.
+        private static string HorizontalLine = "═══════════════════════════════════════════════════════════════════════════════";
         
         private static void Main(string[] args)
         {
             Console.Title = "Digital Väckarklocka - Nivå A";
 
-            // Instantiates the default Alarmclock-object which will be used in the different tests below (in various forms/with various values/modifies depending on each test).
-            AlarmClock alarmclock = new AlarmClock();
-
-            // 1: Test of the default constructor to verify the status of the new object - the string "0:00 (0:00)" corresponding to the object's value should be presented.
             ViewTestHeader("Test 1.\nTest av standardkonstruktorn.");
+            AlarmClock alarmclock = new AlarmClock();
             Console.WriteLine(alarmclock.ToString());
             Console.WriteLine();
         
-            // 2: Test of the constructor with 2 parameters.
-            // Use of the arguments 9 and 42 (9, 42) when instantiating a new object and the time to be presented is 9:42 (0:00).
             ViewTestHeader("Test 2.\nTest av konstruktorn med två parametrar, (9, 42).");
             alarmclock = new AlarmClock(9, 42);
             Console.WriteLine(alarmclock.ToString());
             Console.WriteLine();
 
-            // 3: Constructor with 4 parameters.
-            // Use arg's: 13, 24, 7 and 35. Time to be presented: 13:24 (7:35).
             ViewTestHeader("Test 3.\nTest av konstruktorn med fyra parametrar, (13, 24, 7, 35).");
             alarmclock = new AlarmClock(13, 24, 7, 35);
             Console.WriteLine(alarmclock.ToString());
             Console.WriteLine();
 
-            // 4: Test of the meth TickTock() making the clock turn 1 min.
-            // Sets an existing AC-obj to 23:58 and let it run for 13 min. A list of 13 time stamps should be presented where the mins increase with 1 for each time stamp.
-            // The hours should switch from 23 to 0 and when it turns to 0 it should be presented with just the one digit but the mins with the commencing 0.
             ViewTestHeader("Test 4.\nStäller befintligt AlarmClock-objekt till 23:58 och låter det gå 13 minuter.");
             alarmclock.Hour = 23;
             alarmclock.Minute = 58;
             Run(alarmclock, 13);
 
-            // 5: Sets the time of an existing AC-obj to 6:12, the alarm to 6:15 and makes it run for 6 min. The code should indicate when the alarm is being triggered.
             ViewTestHeader("Test 5.\nStäller befintligt AlarmClock-objekt till tiden 6:12 och alarmtiden till 6:15 \noch låter det gå 6 minuter.");
             alarmclock.Hour = 6;
             alarmclock.Minute = 12;
             alarmclock.AlarmHour = 6;
             alarmclock.AlarmMinute = 15;
             Run(alarmclock, 6);
-
-            // 6: Test of the properties verifying exceptions being thrown when time/alarm is being assigned invalid values.
+            
             ViewTestHeader("Test 6.\nTestar egenskaperna så att undantag kastas då tid och alarmtid tilldelas \nfelaktiga värden.");
 
             try
@@ -95,7 +87,6 @@ namespace _1DV402.S2.L02A
             }
             Console.WriteLine();
 
-            // 7: Test of constructors making sure exceptions are being thrown when time/alarm is being assigned invalid values.
             ViewTestHeader("Test 7.\nTestar konstruktorer så att undantag kastas då tid och alarmtid tilldelas \nfelaktiga värden.");
             
             try
@@ -135,16 +126,17 @@ namespace _1DV402.S2.L02A
             }
         }
 
-        // Method with 2 parameters.
-        // 1st one: a reference to an AlarmClock object.
-        // 2nd one: the amount of minutes an AC-obj should run (by letting an object make numerous calls to the TickTock-method through an iteration).
         private static void Run(AlarmClock ac, int minutes)
         {
             Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(" ╔═════════════════════════════════════╗ ");
             Console.WriteLine(" ║      Väckarklockan URLED (TM)       ║ ");
-            Console.Write(" ║       "); Console.ForegroundColor = ConsoleColor.Gray; Console.Write("Modellnr.: 1DV402S2L2A"); Console.ForegroundColor = ConsoleColor.White; Console.Write("        ║ \n");
+            Console.Write(" ║       "); 
+            Console.ForegroundColor = ConsoleColor.Gray; 
+            Console.Write("Modellnr.: 1DV402S2L2A"); 
+            Console.ForegroundColor = ConsoleColor.White; 
+            Console.Write("        ║ \n");
             Console.WriteLine(" ╚═════════════════════════════════════╝ ");
             Console.ResetColor();
 
@@ -165,7 +157,6 @@ namespace _1DV402.S2.L02A
             Console.WriteLine();
         }
 
-        // Method which receives an exception in form of an argument and presents it (the error messages from the tests). 
         private static void ViewErrorMessage(string message)
         {
             Console.BackgroundColor = ConsoleColor.Red;
@@ -174,7 +165,6 @@ namespace _1DV402.S2.L02A
             Console.ResetColor();
         }
 
-        // Method which receives a string as an argument and presents it (the headers of each test).
         private static void ViewTestHeader(string header)
         {
             Console.WriteLine(HorizontalLine);
